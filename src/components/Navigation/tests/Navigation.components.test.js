a02ed937b3c8f787d5bf19af01d1f117c62a2c2d
lwrooblewski <l.wrooblewski@gmail.com>
2020-03-22 18:02:15 +0100

    test(navigation): add snapshot test

---
+++ src/components/Navigation/tests/Navigation.components.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import renderer from 'react-test-renderer';
+import Navigation from '../Navigation';
+import routes from '../../../routes';
+
+it('renders correctly', () => {
+	const tree = renderer.create(<Navigation routes={routes} />).toJSON();
+	expect(tree).toMatchSnapshot();
+});
