c37b6509afbb03dbea913b9057d9d2144b83bf6f
lwrooblewski <l.wrooblewski@gmail.com>
2020-03-22 22:57:28 +0100

    test(fix): navigation

--- src/components/Navigation/tests/Navigation.components.test.js
+++ src/components/Navigation/tests/Navigation.components.test.js
@@ -1,9 +1,16 @@
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { LocationProvider } from '@reach/router';
 import Navigation from '../Navigation';
 import routes from '../../../router/routes';
 
 it('renders correctly', () => {
-	const tree = renderer.create(<Navigation routes={routes} />).toJSON();
+	const tree = renderer
+		.create(
+			<LocationProvider>
+				<Navigation routes={routes} />
+			</LocationProvider>,
+		)
+		.toJSON();
 	expect(tree).toMatchSnapshot();
 });
