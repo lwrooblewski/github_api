7cce6134fbbfab47c1c341378abef9c80105b72f
lwrooblewski <l.wrooblewski@gmail.com>
2020-03-24 22:59:33 +0100

    feat(api): add single user method

--- src/utils/api/methods.js
+++ src/utils/api/methods.js
@@ -1,5 +1,8 @@
 import endpoints from './endpoints.json';
+import {createLinkAddress} from "@root/router/Router.actions";
 
 export const Users = ({ since }) => `${endpoints.Users}?since=${since}`;
-export const { User } = endpoints;
+export const User = ({ username }) => createLinkAddress(endpoints.User, {
+    username,
+});
 export const { SearchUser } = endpoints;
